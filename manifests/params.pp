49eea840998c0543ef8a37233fa02c469572907a
Pedro González Serrano <niteman.es@gmail.com>
2014-04-03 12:38:27 +0200

    suport changing mail destination

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,8 @@ class logwatch::params (
   $log_dir          = '',
   $multiple_hosts   = false,
   $detail_level     = 'Low',
-  $numeric_addreses = false
+  $numeric_addreses = false,
+  $mail_to          = '',
   # TO-DO logtomail or file
 ) {
   case $::operatingsystem {
